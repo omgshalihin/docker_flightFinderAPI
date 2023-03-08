19cbaac99628f236934424c57fbefbd9f1080be8
omgshalihin <shalihinsgp@gmail.com>
2023-03-08 11:55:45 +0100

    first commit

---
+++ src/main/java/com/salt/flightFinderApi/FlightFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.salt.flightFinderApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightFinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightFinderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salt/flightFinderApi/FlightFinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salt.flightFinderApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightFinderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
